0fb6716d6996d24c07856b2773511299d8683766
Antonio Felipe <felipeduarte301@hotmail.com>
2019-08-03 02:34:40 -0300

    Finish

--- js/programa.js
+++ js/programa.js
@@ -1,39 +1,45 @@
-function resultado(){
-    document.getElementById("Resposta").innerHTML = "";
-    var numero = Number(document.getElementById("numero"));
-    for (let index = 0; index <= numero; index++) {
-        if ((index%3 == 0)&&(index%5 == 0)) {
-            multiploAmbos();
-        } else if(index%3 == 0){
-            multiplo3();
-        } else if(index%5 == 0){
-            multiplo5();
-        } else {
-            naoMultiplo(index);
-        }
-    }
-}
+var core = {
+	message : document.getElementById('Resposta'),
+	idNumber : "numero",
+	number : 0,
+	number3: 3,
+	number5 : 5,
+	messageMultiple3 : "Jovens",
+	messageMultiple5 : "Gênios",
+	messageMultiple3and5 : "Jovens Gênios",
+	colorMultiple3 : "rgb(255,204,0)",
+	colorMultiple5 : "rgb(21,88,88)",
+	colorMultiple3and5 : "rgb(180,255,201)",
+	colorDefault : "rgb(114,0,255)",
+	numberIsInvalid : "Favor insira um número",
 
-function multiplo3(){
-    var exibir = document.getElementById("Resposta");
-    exibir.style.color = "rgb(255,204,0);"
-    exibir.innerHTML = "<h3> Jovens</h3>";
-}
+	multiple : function(value, color){
+		var core = this;
+		core.print(`<h3 style="color:${color}">${value}</h3>`);
+	},
+	print: function(value){
+		var core = this;
+		core.message.innerHTML += value;
+	},
+	result : function(){
+		var core = this;
+		core.message.innerHTML = "";
+	    core.number = document.getElementById(core.idNumber).value;
+	    if(!core.number){
+	    	core.print(`<h3>${core.numberIsInvalid}</h3>`);
+	    	return;
+	    }
 
-function multiplo5(){
-    var exibir = document.getElementById("Resposta");
-    exibir.style.color = "rgb(21,88,88);"
-    exibir.innerHTML = "<h3> Gênios</h3>";
-}
-
-function multiploAmbos(){
-    var exibir = document.getElementById("Resposta");
-    exibir.style.color = "rgb(180,255,201);"
-    exibir.innerHTML = "<h3> JovensGênios</h3>";
-}
-
-function naoMultiplo(numero){
-    var exibir = document.getElementById("Resposta");
-    exibir.style.color = "rgb(114,0,255);"
-    exibir.innerHTML = "<h3>"+" "+ numero +"</h3>";
+	    for (var index = 0; index <= core.number; index++) {
+	        if ( index%core.number3 == 0 && index%core.number5 == 0) {
+	            core.multiple(core.messageMultiple3and5, core.colorMultiple3and5);
+	        } else if(index%core.number3 == 0){
+	            core.multiple(core.messageMultiple3, core.colorMultiple3);
+	        } else if(index%core.number5 == 0){
+	            core.multiple(core.messageMultiple5, core.colorMultiple5);
+	        } else {
+	            core.multiple(index, core.colorDefault);
+	        }
+	    }	
+	}
 }
